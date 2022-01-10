575c0d306105ff220736b98bbca07d970c047e6e
leanaAlq <leana.a.q@gmail.com>
2022-01-11 00:05:09 +0300

    search working & styling

--- src/components/core/searchHotels/searchHotels.tsx
+++ src/components/core/searchHotels/searchHotels.tsx
@@ -11,7 +11,7 @@ import SearchInput from "../../searchInput/searchInput";
 const SearchHotels = () => {
 	const [from, setFrom] = useState<Date | null>(new Date());
 	const [to, setTo] = useState<Date | null>(new Date());
-	const [hotels, setHotels] = useState([]);
+	const [hotels, setHotels]: any[] = useState([]);
 	const [ShowHotels, setShowHotels] = useState(false);
 	const [diff, setDiff] = useState(0);
 	const hotelsService: any = new HotelsService();
@@ -30,8 +30,6 @@ const SearchHotels = () => {
 		const diffInMs = new Date(to) - new Date(from);
 		const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
 		// @ts-ignore
-		console.log("diffInDays", Math.round(diffInDays));
-		// @ts-ignore
 		setDiff(Math.round(diffInDays));
 	};
 	const getHotels = () => {
@@ -46,12 +44,22 @@ const SearchHotels = () => {
 				setShowHotels(true);
 			});
 	};
+
+	const filterHotels = (e: any) => {
+		console.log(e.target.value);
+		let list: any[] = [...hotels];
+		list = list.filter((hotel: any) => hotel.name.includes(e.target.value));
+		console.log(list);
+
+		setHotels(list);
+	};
+
 	return ShowHotels ? (
 		<div className="hotelsList">
 			{hotels && !!hotels.length ? (
 				<div className="hotels">
 					<div className="actions">
-						<SearchInput onChange={() => { }} />
+						<SearchInput onChange={filterHotels} />
 						<div className="price-range">
 							<span>Price Range</span>
 							<Slider
@@ -63,7 +71,6 @@ const SearchHotels = () => {
 								max={1000}
 							/>
 						</div>
-
 					</div>
 					<div className="list-container">
 						<div className="header">
@@ -81,19 +88,19 @@ const SearchHotels = () => {
 					</div>
 				</div>
 			) : (
-					<div>No results Found!</div>
-				)}
+				<div className="not-found">No results Found!</div>
+			)}
 		</div>
 	) : (
-			<div className="SearchHotels">
-				<div className="background"></div>
-				<DateInput label="from" value={from} onChange={onFromChange} />
-				<DateInput label="to" value={to} onChange={onToChange} />
-				<Button className="searchBtn" onClick={getHotels} variant="outlined">
-					Search
+		<div className="SearchHotels">
+			<div className="background"></div>
+			<DateInput label="from" value={from} onChange={onFromChange} />
+			<DateInput label="to" value={to} onChange={onToChange} />
+			<Button className="searchBtn" onClick={getHotels} variant="outlined">
+				Search
 			</Button>
-			</div>
-		);
+		</div>
+	);
 };
 
 export default SearchHotels;
